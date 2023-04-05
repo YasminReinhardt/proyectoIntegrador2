29ac49c164986944de38ae48160decd9145b7d0b
YasminReinhardt <yreinhardt@udesa.edu.ar>
2023-04-05 18:07:29 -0400

    routas y controladores

---
+++ db/data.js
@@ -0,0 +1,93 @@
+const data= {
+    usuario: [ //lo que te pide al ingresar (inventado)
+        {
+            nombre: 'renata', 
+            contrasena: '2003', 
+            usuario: 'reni', 
+            fechaNacimiento: '20/03/1999', 
+            documento: '45580174', 
+            fotoPerfil: '', 
+
+        }
+    ],
+    productos: [
+        {
+            imagen: '',
+            nombreProducto: 'Pelota',
+            descripcion: 'Pelota del Mundial',
+            fechaCarga:'20/02/1994',
+
+        },
+        {
+            imagen: '',
+            nombreProducto: 'Pelota',
+            descripcion: 'Pelota del Mundial',
+            fechaCarga:'20/02/1994',
+
+        },       
+        {
+            imagen: '',
+            nombreProducto: 'Pelota',
+            descripcion: 'Pelota del Mundial',
+            fechaCarga:'20/02/1994',
+
+        },        {
+            imagen: '',
+            nombreProducto: 'Pelota',
+            descripcion: 'Pelota del Mundial',
+            fechaCarga:'20/02/1994',
+
+        },       
+        {
+            imagen: '',
+            nombreProducto: 'Pelota',
+            descripcion: 'Pelota del Mundial',
+            fechaCarga:'20/02/1994',
+
+        },       
+        {
+            imagen: '',
+            nombreProducto: 'Pelota',
+            descripcion: 'Pelota del Mundial',
+            fechaCarga:'20/02/1994',
+
+        },        
+        {
+            imagen: '',
+            nombreProducto: 'Pelota',
+            descripcion: 'Pelota del Mundial',
+            fechaCarga:'20/02/1994',
+
+        },       
+        {
+            imagen: '',
+            nombreProducto: 'Pelota',
+            descripcion: 'Pelota del Mundial',
+            fechaCarga:'20/02/1994',
+
+        } ,       
+        {
+            imagen: '',
+            nombreProducto: 'Pelota',
+            descripcion: 'Pelota del Mundial',
+            fechaCarga:'20/02/1994',
+
+        },  
+              {
+            imagen: '',
+            nombreProducto: 'Pelota',
+            descripcion: 'Pelota del Mundial',
+            fechaCarga:'20/02/1994',
+
+        },
+    ],
+    comentarios: [
+        {
+            nombreDeUsuario: '', 
+            textoDelComentario: '',
+            imagenPerfil: '',
+        }
+    ],
+
+}
+module.exports= data
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const index= require ('../controllers/index.controllers')
+
+// creando las rutas con los controllers 
+router.get ('/', index.index)
+router.get ('/login', index.login)
+router.get ('/register',index.register)
+router.get ()//falta uno
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
