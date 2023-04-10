72bf1a2a17727947a3c2bfc8f1b3910c24cc6afb
YasminReinhardt <yreinhardt@udesa.edu.ar>
2023-04-10 15:12:46 -0400

    arreglo index

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const index= require ('../controllers/index.controllers')
+const index= require ('../controllers/indexControllers')
 
 // creando las rutas con los controllers (explicaron en clase)
-router.get ('/', index.index)
-router.get ('/login', index.login)
-router.get ('/register',index.register)
+router.get('/', index.index)
+
 
 module.exports = router;
