f16efc925796b0908e7dd4f3e4e2eaa6ee77f1e5
luciacupito <lcupito@udesa.edu.ar>
2023-04-10 16:50:27 -0300

    arreglando

--- data/schema.sql
+++ data/schema.sql
@@ -1,20 +1,20 @@
 CREATE TABLE usuarios (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  usuario VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
-  password VARCHAR(255) NOT NULL,
-  photo_url VARCHAR(255),
-  birthdate DATE,
-  dni INT,
+  id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  usuario VARCHAR(55) NOT NULL UNIQUE,
+  email VARCHAR(55) NOT NULL UNIQUE,
+  password VARCHAR(55) NOT NULL,
+  photo VARCHAR(55),
+  birthdate DATE NOT NULL,
+  dni INT NOT NULL UNIQUE,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   deleted_at TIMESTAMP NULL DEFAULT NULL
-);
+ );
 
 CREATE TABLE productos (
-  id INT PRIMARY KEY AUTO_INCREMENT,
-  usuario_id INT,
-  nombre VARCHAR(255) NOT NULL,
+  id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  usuario_id INT NOT NULL,
+  nombre VARCHAR(55) NOT NULL,
   descripcion TEXT,
   created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -23,7 +23,7 @@ CREATE TABLE productos (
 );
 
 CREATE TABLE comentarios (
-  id INT PRIMARY KEY AUTO_INCREMENT,
+  id INT UNSIGNED PRIMARY KEY NOT NULL AUTO_INCREMENT,
   productos_id INT,
   usuario_id INT,
   texto TEXT,
@@ -34,7 +34,6 @@ CREATE TABLE comentarios (
   FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
 );
 
--- Users
 insert into usuarios (usuario, email, password, dni, birthdate)
     values('lula', 'lula@gmail.com', '1234', 12345678, '1990-01-01');
 
@@ -50,7 +49,6 @@ insert into usuarios (usuario, email, password, dni, birthdate)
 insert into usuarios (usuario, email, password, dni, birthdate)
     values('lucas', 'lucas@gmail.com', '1234', 12345678, '1990-01-01');
 
--- Productos
 insert into productos (usuario_id, nombre, descripcion)
     values(1, 'Pelota', 'Pelota del Mundial');
 
@@ -81,8 +79,6 @@ insert into productos (usuario_id, nombre, descripcion)
 insert into productos (usuario_id, nombre, descripcion)
     values(5, 'Pantalon', 'Pantalon de la Seleccion');
 
-
--- Comentarios
 insert into comentarios (productos_id, usuario_id, texto)
     values(1, 1, 'Muy buena pelota');
 
