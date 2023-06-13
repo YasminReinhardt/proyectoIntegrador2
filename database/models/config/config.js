245f299faf207597e71fac04511e68fa3bc9fc56
YasminReinhardt <yreinhardt@udesa.edu.ar>
2023-06-13 11:14:01 -0400

    ...

--- database/models/config/config.js
+++ database/models/config/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     "development": {
       "username": "root",
-      "password": "root",
+      "password": null,
       "database": "prog_practica1",
       "host": "127.0.0.1",
       "dialect": "mysql"
